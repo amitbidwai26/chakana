4f498edea3619f94b4507473ccb16006f662e803
amitbidwai <amitbidwai.26@gmail.com>
2016-12-18 19:04:29 +0530

    api added

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
+var bodyParcer = require ('bodyParcer');
+var mongoose = require ('mongoose');
+
+
+var url ='mongodb://user12:user12@ds035006.mlab.com:35006/healthapp2017'
+var db = mongoose.connect (url);
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -12,13 +18,24 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
-app.get('/beverages',function(request, response){
+app.get('/api', function(request, response) {
+  response.render('pages/api');
+});
 
+app.get('/beverages',function(request, response){
 response.send("ping");
 });
 
+
+
+
+
+
+
+
+
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('Chakhna app is running on port', app.get('port'));
 });
 
 
