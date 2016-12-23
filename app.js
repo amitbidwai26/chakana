f55173af4142e858f59894d84ace56722c4192cb
Amit Bidwai <amitbidwai.26@gmail.com>
2016-12-23 17:05:17 +0530

    check mongoose in server

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 //var bodyParcer = require ('bodyParcer');
-//var mongoose = require ('mongoose');
+var mongoose = require ('mongoose');
 
 
-//var url ='mongodb://user12:user12@ds035006.mlab.com:35006/healthapp2017'
-//var db = mongoose.connect (url);
+var url ='mongodb://user12:user12@ds035006.mlab.com:35006/healthapp2017'
+var db = mongoose.connect (url);
 
 app.set('port', (process.env.PORT || 5000));
 
